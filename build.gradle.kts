af499f604cf4c124d8520965bbe09d2173a57b73
punditke <121955010+punditke@users.noreply.github.com>
2026-06-18 13:56:45 +0300

    Add KSP plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
 }
